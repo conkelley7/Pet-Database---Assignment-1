8508d9cf2e868563f97c8e5902d2faaab1e0dffb
conke <conke@ConnorsPC>
2024-11-01 13:37:19 -0500

    Edited for version 3 of Pet Database. Added functionalities for updating and removing pets.

--- src/main/java/petdatabase/Main.java
+++ src/main/java/petdatabase/Main.java
@@ -22,9 +22,11 @@ public class Main {
             System.out.println("What would you like to do?");
             System.out.println("1) View all pets");
             System.out.println("2) Add more pets");
-            System.out.println("3) Search pets by name");
-            System.out.println("4) Search pets by age");
-            System.out.println("5) Exit program");
+            System.out.println("3) Update a pet");
+            System.out.println("4) Remove a pet");
+            System.out.println("5) Search pets by name");
+            System.out.println("6) Search pets by age");
+            System.out.println("7) Exit program");
             System.out.print("Your choice: ");
             input = scnr.nextLine();
 
@@ -52,21 +54,36 @@ public class Main {
                         }
                     }
                     break;
-                    
+                // Updating pets
                 case "3":
+                    System.out.print("Enter pet ID to update: ");
+                    int updateId = Integer.parseInt(scnr.nextLine());
+                    System.out.print("Enter new name and age: ");
+                    String[] updateParts = scnr.nextLine().split(" ");
+                    database.updatePet(updateId, updateParts[0], Integer.parseInt(updateParts[1]));
+                    break;
+                // Removing pets
+                case "4":
+                    System.out.print("Enter pet ID to remove: ");
+                    int removeId = Integer.parseInt(scnr.nextLine());
+                    database.removePet(removeId);
+                    break;
+                // Search pets by name    
+                case "5":
                     System.out.print("Enter pet name to search: ");
                     String searchName = scnr.nextLine();
                     ArrayList<Pet> foundByName = database.searchByName(searchName);
                     database.displayPets(foundByName);
                     break;
-                case "4":
+                // Search pets by age
+                case "6":
                     System.out.print("Enter pet age to search: ");
                     int searchAge = Integer.parseInt(scnr.nextLine());
                     ArrayList<Pet> foundByAge = database.searchByAge(searchAge);
                     database.displayPets(foundByAge);
                     break;
                 // Exiting program
-                case "5":
+                case "7":
                     System.out.println("Exiting program.");
                     scnr.close();
                     return;
