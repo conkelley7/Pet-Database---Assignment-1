91b9f0fd6965ba58b07fd47b1ef128217eead786
conke <conke@ConnorsPC>
2024-11-07 12:11:34 -0600

    Edited for version 4 of Pet Database. Added functionalities for saving/loading from file and additional error handling.

--- src/main/java/petdatabase/PetDatabase.java
+++ src/main/java/petdatabase/PetDatabase.java
@@ -2,20 +2,32 @@ package petdatabase;
 
 /**
  * This class contains methods for managing database.
- * Version 1: Contains methods for adding and displaying pets.
- * Future version will expand capabilities.
  * @author conke
  */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PetDatabase {
     private ArrayList<Pet> pets;
+    private final String FILENAME = "pets.txt";
+    private final int MAX_PETS = 5;
 
     public PetDatabase() {
         pets = new ArrayList<>();
+        loadPetsFromFile();
     }
 
     public void addPet(String name, int age) {
+        if (pets.size() >= MAX_PETS) {
+            System.out.println("Error: Database is full. 5 pet limit reached.");
+        }
+        if (age < 1 || age > 20) {
+            System.out.println("Error: Age must be between 1 and 20.");
+        }
         pets.add(new Pet(name, age));
     }
     
@@ -65,5 +77,33 @@ public class PetDatabase {
             }
         }
         return result;
+    }
+    
+    private void loadPetsFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILENAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("Loading pets from file.");
+                String [] parts = line.split(",");
+                if (parts.length == 2) {
+                    String name = parts[0].trim();
+                    int age = Integer.parseInt(parts[1].trim());
+                    addPet(name, age);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Could not load pets from file.");
+        }
+    }
+    
+    public void savePetsToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME))) {
+            for (Pet pet: pets) {
+                writer.write(pet.getName() + "," + pet.getAge());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Could not save pets to file");
+        }
     }
 }
